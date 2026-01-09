e0c2521bd15022ef3b2acf60d09ec20633334c71
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-01-09 20:37:06 -0300

    Criando configuração inicial do servidor

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import http from "node:http"
+
+const PORT = 3333
+
+const server = http.createServer((req, res) => {
+    
+})
+
+server.listen(PORT, () => {
+    console.log("Servidor rodando!")
+})
